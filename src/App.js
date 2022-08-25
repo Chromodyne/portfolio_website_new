616562fbbd0c8fe007d9891bf34b7f109e4b52fc
chromodyne <dsinitiere@outlook.com>
2022-08-25 15:05:54 -0700

    Relative links changed from anchors to router links.

--- src/App.js
+++ src/App.js
@@ -3,26 +3,23 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useLocation
 } from "react-router-dom";
 import Navbar from "./components/Navbar"
 import About from './components/About';
-import React, { useState } from 'react';
+import React from 'react';
 import Portfolio from './components/Portfolio';
 import Footer from "./components/Footer"
 import Contact from "./components/Contact"
 
 const AppRouter = () => {
 
-  const location = useLocation();
-
   return (
     <>
       <Navbar />
-        <Routes location={location}>
-          <Route path="/" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
+        <Routes>
+          <Route path="/portfolio_website_new" element={<About />} />
+          <Route path="/portfolio_website_new/portfolio" element={<Portfolio />} />
+          <Route path="/portfolio_website_new/contact" element={<Contact />} />
         </Routes>
       <Footer />
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "flowbite";
+import { Link } from "react-router-dom";
 
 function NavBar() {
 
@@ -15,19 +16,19 @@ function NavBar() {
           <div class="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul class="flex flex-col p-4 mt-4 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-secondary dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a href="/" class="block py-2 pr-4 pl-3 text-white rounded md:hover:text-quaternary md:bg-transparent md:p-0 dark:text-white" aria-current="page">About</a>
+                <Link to="/portfolio_website_new" className="block py-2 pr-4 pl-3 text-white rounded md:hover:text-quaternary md:bg-transparent md:p-0 dark:text-white" aria-current="page">About</Link>
               </li>
               <li>
-                <a href="/portfolio" class="block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-quaternary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Portfolio</a>
+              <Link to="/portfolio_website_new/portfolio" className="block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-quaternary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Portfolio</Link>
               </li>
               <li>
-                <a href="https://github.com/Chromodyne" target="_blank" rel="noreferrer" class="block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-quaternary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">GitHub</a>
+                <a href="https://github.com/Chromodyne" target="_blank" rel="noreferrer" className="block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-quaternary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">GitHub</a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/devin-sinitiere-78224295/" target="_blank" rel="noreferrer" class="block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-quaternary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/devin-sinitiere-78224295/" target="_blank" rel="noreferrer" className="block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-quaternary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">LinkedIn</a>
               </li>
               <li>
-                <a href="/contact" class="block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-quaternary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                <Link to="/portfolio_website_new/contact" className="block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-quaternary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
               </li>
             </ul>
           </div>
